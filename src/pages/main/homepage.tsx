faca3bb503d54b41372ab62bbe93708c8f2a6878
Erick Tran <thelittlebotengineer@gmail.com>
2024-06-24 07:41:43 -0700

    3-feat. Finished part of homepage

--- src/pages/main/homepage.tsx
+++ src/pages/main/homepage.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import '../../assets/scss/homepage.scss';
+import UserData from '../../assets/ts/interfaces';
 
 const HomePage: React.FC = (): React.JSX.Element => {
-    return (
-        <section id="homepage">
+    const [data, setData] = React.useState<UserData | null>({ username: 'Erick Tran' });
 
+    React.useEffect(() => {
+        try {
+            setUserData({ username: 'Erick Tran' });
+            // getUserData().then((result: UserData | null | undefined) => {
+            //     setData(result);
+            // });
+        } catch (error: unknown) {
+            console.error('Error:', error as string);
+        }
+    }, []);
+
+    const setUserData = (data: UserData | null): void => {
+        setData(data);
+    }
+
+
+    return (
+        <section id="home">
+            <div id="container">
+                <div id="content">
+                    <h1>Welcome {data!.username}!</h1>
+                    <p>This is a simple homepage.</p>
+                </div>
+            </div>
         </section>
     )
 }
