376ed7bc5f95c4bff6d5201de14f606f4a45cfdf
Erick Tran <thelittlebotengineer@gmail.com>
2024-06-24 11:51:12 -0700

    7-bug. Fixed Google Strategy Error

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,21 @@
 import express, { Express } from "express";
-import { APP_HOSTNAME, SERVER_PORT, CLIENT_ID } from "./modules/env";
+import {
+  APP_HOSTNAME,
+  SERVER_PORT,
+  CLIENT_ID,
+  CLIENT_SECRET,
+} from "./modules/env";
 import session from "express-session";
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
 import connectMongoDBSession from "connect-mongodb-session";
-import cors from 'cors';
+import cors from "cors";
 import dotenv from "dotenv";
 import "./setupProxy";
-import { generateRandomNumber, permanentEncryptPassword } from "./modules/encryption";
+import {
+  generateRandomNumber,
+  permanentEncryptPassword,
+} from "./modules/encryption";
 
 const MongoDBStore = connectMongoDBSession(session);
 
@@ -23,24 +31,26 @@ app.set("trust proxy", true);
 dotenv.config({ path: "./modules/credentials.env.local" });
 
 // Secret
-const SECRET: string = permanentEncryptPassword(generateRandomNumber(256, "alphanumeric").toString());
+const SECRET: string = permanentEncryptPassword(
+  generateRandomNumber(256, "alphanumeric").toString()
+);
 
-app.use(session({
-  secret: SECRET,
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoDBStore({
-    uri: process.env.MONGODB_URI!,
-    databaseName: process.env.CLIENT_DB,
-    collection: 'sessions',
-    expires: 1000 * 60 * 60 * 24 * 31, // 1 month
-    autoRemove: 'interval',
-    autoRemoveInterval: 10 * 1000 // interval between expired sessions check in milliseconds
-  }),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 3.5, // 3.5 days
-  }
-}));
+app.use(
+  session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoDBStore({
+      uri: process.env.MONGODB_URI!,
+      databaseName: process.env.CLIENT_DB,
+      collection: "sessions",
+      expires: 1000 * 60 * 60 * 24 * 31, // 1 month
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 3.5, // 3.5 days
+    },
+  })
+);
 
 // Passport
 app.use(passport.initialize());
@@ -53,14 +63,30 @@ passport.serializeUser((user, done) => {
 
 // Requests for the Google OAuth2.0 login page
 passport.deserializeUser((user, done) => {
-  return done(null, user);
+  return done(null, user as false | Express.User | null | undefined);
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+        }
+    )
+);
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google/callback', passport.authenticate('google'), async (req, res) => {
+    res.redirect('/');
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running at http://${APP_HOSTNAME}:${SERVER_PORT}`);
