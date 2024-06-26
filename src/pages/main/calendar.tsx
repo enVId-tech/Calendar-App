ae25be4d52c0bbf32f001d6fa8c6615f0d9a265d
Erick Tran <thelittlebotengineer@gmail.com>
2024-06-25 21:01:13 -0700

    26-feat. Added to calendar and events

--- src/pages/main/calendar.tsx
+++ src/pages/main/calendar.tsx
@@ -2,6 +2,32 @@ import React from 'react';
 import '../../assets/scss/calendar.scss';
 
 const CalendarPage: React.FC = () => {
+    const [year, setYear] = React.useState<number>(new Date().getFullYear());
+    const [month, setMonth] = React.useState<number>(new Date().getMonth() + 1);
+    const [days, setDays] = React.useState<number[]>([1, 2, 3, 4, 5, 6, 7]);
+
+    const getEvents = async (year: number, month: number): Promise<void> => {
+        const jsonData = {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify({
+                "year": year,
+                "month": month
+            })
+        }
+
+        const response: Response = await fetch('/api/calendar/events', jsonData);
+        const data: number[] = await response.json();
+
+        setDays(data);
+    }
+
+    React.useEffect(() => {
+        getEvents(year, month);
+    }, [year, month]);
+
     return (
         <section id="calendar">
             <div id="header">
--- src/pages/main/events.tsx
+++ src/pages/main/events.tsx
@@ -2,9 +2,45 @@ import React from 'react';
 import '../../assets/scss/events.scss';
 
 const EventsPage: React.FC = (): React.JSX.Element => {
+    const sendEvents = async () => {
+        try {
+            const dataJson = {
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify({
+                    "userId": document.cookie.split("=")[1],
+                    "events": []
+                })
+            }
+
+            const response = await fetch('/api/get/events', dataJson);
+            const data = await response.json();
+
+            if (data.error) {
+                console.error(data.error);
+            } else {
+                console.log(data.message);
+            }
+        } catch (error: unknown) {
+            console.error('Error:', error as string);
+        }
     return (
         <section id="events">
-            <h1>Events</h1>
+            <div id="container">
+                <div id="content">
+                    <h1>Events</h1>
+                </div>
+                <div id="events">
+                    <div id="left">
+
+                    </div>
+                    <div id="right">
+
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
