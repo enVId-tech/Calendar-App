1c99600e7de4b43e488f3657238923aba230e31d
Erick Tran <thelittlebotengineer@gmail.com>
2024-06-27 07:33:08 -0700

    41-feat. Added content to events page

--- src/pages/main/events.tsx
+++ src/pages/main/events.tsx
@@ -29,6 +29,37 @@ const EventsPage: React.FC = (): React.JSX.Element => {
         }
     }
 
+    const getEvents = () => {
+        try {
+            const dataJson = {
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify({
+                    "userId": document.cookie.split("=")[1]
+                })
+            }
+
+            fetch('/api/get/events', dataJson)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        console.error(data.error);
+                    } else {
+                        console.log(data.message);
+                    }
+                }
+            );
+        } catch (error: unknown) {
+            console.error('Error:', error as string);
+        }
+    }
+
+    React.useEffect(() => {
+        getEvents();
+    }, []);
+
     return (
         <section id="events">
             <Sidebar />
@@ -36,12 +67,29 @@ const EventsPage: React.FC = (): React.JSX.Element => {
                 <div id="content">
                     <h1>Events</h1>
                 </div>
-                <div id="events">
+                <div id="eventsLists">
                     <div id="left">
-
+                        <p id="eventList">Event List</p>
+                        <div id="eventListContent">
+                            <div className="event">
+                                <h1>Event Name</h1>
+                                <p>Event Date</p>
+                                <p>Event Time</p>
+                                <p>Event Location</p>
+                                <p>Event Description</p>
+                            </div>
+                        </div>
                     </div>
                     <div id="right">
-
+                        <p id="eventCreate">Create Event</p>
+                        <form id="eventForm">
+                            <input type="text" id="eventName" placeholder="Event Name" />
+                            <input type="text" id="eventDate" placeholder="Event Date" />
+                            <input type="text" id="eventTime" placeholder="Event Time" />
+                            <input type="text" id="eventLocation" placeholder="Event Location" />
+                            <input type="text" id="eventDescription" placeholder="Event Description" />
+                            <button type="button" onClick={() => sendEvents()}>Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
