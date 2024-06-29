3ee84b39af3ec10998e14ff95b5bb138d9dbb2c7
Erick Tran <thelittlebotengineer@gmail.com>
2024-06-29 15:14:10 -0700

    71-feat. Added new event stuff for several pages

--- src/pages/main/events.tsx
+++ src/pages/main/events.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import '../../assets/scss/events.scss';
 import Sidebar from '../../assets/components/sidebar';
--- src/pages/main/homepage.tsx
+++ src/pages/main/homepage.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import '../../assets/scss/homepage.scss';
-import { UserData } from '../../assets/ts/interfaces';
+import { EventData, UserData } from '../../assets/ts/interfaces';
 import getUserData from '../../assets/ts/getUserData';
 import Sidebar from '../../assets/components/sidebar';
 import FormatCalendar from '../../assets/components/format-calendar';
 
 const HomePage: React.FC = (): React.JSX.Element => {
-    const [data, setData] = React.useState<UserData | null | undefined>({});
+    const [data, setData] = React.useState<UserData | null | undefined>();
+    const [events, setEvents] = React.useState<EventData[]>([]);
 
     const getEvents = async () => {
         try {
@@ -34,6 +37,28 @@ const HomePage: React.FC = (): React.JSX.Element => {
             } else {
                 console.log(data.message);
             }
+
+            setEvents(data);
+
+        } catch (error: unknown) {
+            console.error('Error:', error as string);
+        }
+    }
+
+    const userData = async () => {
+        try {
+            const userId: string | null = document.cookie.split(';')[1].split('=')[1];
+            
+            const userData: UserData[] | undefined | null = await getUserData(userId);
+
+            if (!userData) {
+                window.location.href = '/login';
+                return;
+            }
+
+            setData(userData[0]);
+
+            getEvents();
         } catch (error: unknown) {
             console.error('Error:', error as string);
         }
@@ -41,10 +66,7 @@ const HomePage: React.FC = (): React.JSX.Element => {
 
     React.useEffect(() => {
         try {
-            const userId: string | null = document.cookie.split(';')[1].split('=')[1];
-            getUserData(userId).then((result: UserData | null | undefined) => {
-                setData(result[0]);
-            });
+            userData();
         } catch (error: unknown) {
             console.error('Error:', error as string);
         }
